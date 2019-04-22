8e6153b81ea8413d34b40ef9227e04839fe0b90e
Phillip <36668216+Pj1394@users.noreply.github.com>
2019-04-21 20:11:33 -0400

    Add files via upload
    City Object

---
+++ BMPTravel/src/programBackbone/City.java
@@ -0,0 +1,49 @@
+
+public class City {
+	
+	private String cityName;
+	private String cityLocation;
+	private String cityID;
+	
+	public City(){
+		cityName = "Atlanta";
+		cityLocation = "Georgia";
+		cityID ="ATL";
+			
+	}
+	
+	public City(String CityName, String CityLocation, String CityID){
+		CityName = cityName;
+		CityLocation = cityLocation;
+		CityID = cityID;
+	}
+	
+		public String getCityName() {
+			return cityName;
+		}
+		
+		public void setCityName (String cityName) {
+			this.cityName = cityName;
+		}
+		
+		public String getCityLocation(){
+			return cityLocation;
+		}
+		public void setCityLocation (String cityLocation) {
+			this.cityLocation = cityLocation;
+		}
+		
+		public String getCityID() {
+			return cityID;
+		}
+		
+		public void setCityID(String cityID) {
+			this.cityID = cityID;
+		}
+		
+		
+		public String toString() {
+			return "City Name: " + cityName + "\nLocation" + cityLocation + "\nCity ID" + cityID;
+		}
+	}
+
