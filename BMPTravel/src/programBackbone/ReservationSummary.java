003b4b84f7aa61eb1f818824c8dfd0fd3aa594ea
Phillip <36668216+Pj1394@users.noreply.github.com>
2019-05-06 15:05:47 -0400

    Update ReservationSummary.java

--- BMPTravel/src/programBackbone/ReservationSummary.java
+++ BMPTravel/src/programBackbone/ReservationSummary.java
@@ -1,4 +1,29 @@
-
-public class ReservationSummary {
-
-}
+	public static String getSecurityQuestion(String S) {
+		
+		Connection conn = getConnection();
+		
+		String PassStr = "SELECT SecurityQuestion FROM Username WHERE Username = ?";
+		
+		try {
+			PreparedStatement presecurityQuestion = conn.prepareStatement(securityQuestionStr);
+		
+			presecurityQuestion.setString(1, u1.getUser());
+			
+			ResultSet set = preSecurityQuestion.executeQuery();
+			
+			while(set.next()) {
+				
+				Sting securityQuestion;
+				return this.SecurityQuestion;
+			}
+			
+		}
+		catch(NullPointerException npe) {
+			AlertBox.display("Problem", "The Security Question You Are Trying To find Does Not Seem To Exist, try again.");
+			npe.printStackTrace();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
